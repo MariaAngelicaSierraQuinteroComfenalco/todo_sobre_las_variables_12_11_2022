ca70cca58aad6b07faaeb5dabb04eacb0444c3ab
MariaAngelicaSierraQuinteroComfenalco <angelicasierr904@gmail.com>
2022-11-14 12:24:27 -0500

    todo_sobre_las_variables_12_11_2022

---
+++ main.js
@@ -0,0 +1,44 @@
+addEventListener(`DOMContentLoaded`, (e)=>{
+
+    document.querySelector("body").innerHTML = "BIENVENIDOS";
+    document.querySelector("body").innerHTML = "Usario porfavor revisar consola (oprimir tecla f12)";
+
+    
+    let miNombre ="Maria Angelica";   
+    console.group("¿Cuales son tus nombres?");
+    console.log(miNombre);
+    console.groupEnd();
+
+    let miApellido ="Sierra Quintero";   
+    console.group("¿Cuales son tus Apellidos?");
+    console.log(miApellido);
+    console.groupEnd();
+
+    let miEdad = 17;   
+    console.group("¿Cual es tu edad?");
+    console.log(miEdad);
+    console.groupEnd();
+
+    let Estudio =true ;   
+    console.group("¿Eres estudiante?");
+    console.log(Estudio);
+    console.groupEnd();
+
+    let meGraduare ;   
+    console.group("¿Me graduare de mi carrera?");
+    console.log(Megraduare);
+    console.groupEnd();
+
+    let estudiarOtroProgramagrama=null;
+    console.group("¿Como se llama el progrma que estudiare?");
+    console.log(estudiarOtroPrograma);
+    console.groupEnd();
+
+
+let mascota ={Clase:"gata",Nombre:"Hera",Edad:"4 años",Peso: "4kg"}
+
+    console.group("Caracteristicas de tu mascota");
+    console.log(mascota);
+    console.groupEnd();
+
+})
